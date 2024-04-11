a11d5c9d5dd88895b660e6dff475be207c0bab2d
DaniloAndresSP <76256087+DaniloAndresSP@users.noreply.github.com>
2024-04-11 14:08:55 -0500

    feature/recyclear_view DS 20240411
    - Se crea y se implementa el recyclearView con su Adapter: View Holder

--- app/src/main/java/com/example/usersp/MainActivity.kt
+++ app/src/main/java/com/example/usersp/MainActivity.kt
@@ -5,16 +5,34 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.usersp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var userAdapter: UserAdapter
+    private lateinit var linearLayoutManager: RecyclerView.LayoutManager
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        userAdapter = UserAdapter(mutableListOf())
+        linearLayoutManager = LinearLayoutManager(this)
+
+        binding.recyclearView.apply {
+            layoutManager = linearLayoutManager
+            adapter = userAdapter
         }
     }
 }
